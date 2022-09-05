3275bdad9e8c551c79b53d677d63250309551b4c
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-05 14:17:06 -0300

    Implements new fixtures

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 from app import app as application
 from models.base_mixin import db
+from models import Pessoa, Conta
 
 
 @pytest.fixture
@@ -15,3 +16,32 @@ def app():
         yield application
         db.session.remove()
         db.drop_all()
+
+
+
+@pytest.fixture
+def pessoa():
+    """Create Pessoa Model"""
+    pessoa_obj: Pessoa = Pessoa(
+        nome = "Fulano de Tal",
+        cpf = "71966618077",
+        data_nascimento = "1988-01-01",
+    )
+    pessoa_obj.save()
+
+    return pessoa_obj
+
+
+@pytest.fixture
+def conta(pessoa): # pylint: disable=redefined-outer-name
+    """Create Conta Model"""
+
+    conta_obj: Conta = Conta(
+        id_pessoa=pessoa.id_pessoa,
+        limite_saque_diario=1000.00,
+        flag_ativo=True,
+        tipo_conta=1
+    )
+    conta_obj.save()
+
+    return conta_obj
