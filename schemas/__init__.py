2a0518c62a83a1b2539e0a03531f5b47b2e30227
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-05 04:29:07 -0300

    Update schemas module

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,9 +1,11 @@
 """Schema Module"""
-from .conta import AccountIn, AccountOut
+from .account import AccountIn, AccountOut
 from .transacao import TransactionIn, TransactionOut
 from .balance import AccountBalanceOut
 from .deposit import AccountDepositIn, AccountDepositOut
 from .block import AccountBlockOut
+from .withdraw import AccountWithdrawIn, AccountWithdrawOut
+
 
 __all__ = [
     "AccountIn",
@@ -13,4 +15,7 @@ __all__ = [
     "AccountBalanceOut",
     "AccountDepositIn",
     "AccountDepositOut",
+    "AccountBlockOut",
+    "AccountWithdrawIn",
+    "AccountWithdrawOut",
 ]
