569df270a9e006997cc776ab8c51713fa8b812f3
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-05 04:28:42 -0300

    Implements Withdraw endpoint

--- routes/withdraw.py
+++ routes/withdraw.py
@@ -1,6 +1,23 @@
 """Withdraw Module"""
 from app import app
+from models import Conta
+from schemas import AccountWithdrawIn, AccountWithdrawOut
 
-@app.post('/account/<int:account_id>/withdraw/')
-def withdraw(account_id):
-    return {}
+
+
+@app.post("/account/<int:account_id>/withdraw/")
+@app.input(AccountWithdrawIn)
+@app.output(AccountWithdrawOut)
+def withdraw(account_id, data):
+    """With draw money"""
+
+    account: Conta = Conta.query.filter_by(id_conta=account_id).first()
+    if account.withdraw_money(data["valor"]):
+        message: str = "success"
+    else:
+        message: str = "insufficient funds"
+
+    return {
+        "message": message,
+        "saldo": account.saldo
+    }
---
+++ schemas/withdraw.py
@@ -0,0 +1,16 @@
+"""Withdraw account schema module"""
+from apiflask import Schema, fields
+
+
+class AccountWithdrawBase(Schema):
+    """Schema Base AccountWithdraw"""
+
+class AccountWithdrawIn(AccountWithdrawBase):
+    """Schema Input AccountWithdraw"""
+    valor = fields.Float()
+
+
+class AccountWithdrawOut(AccountWithdrawBase):
+    """Schema Output AccountWithdraw"""
+    saldo = fields.Float()
+    message = fields.String()
