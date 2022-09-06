ab7a252ff533b54ae8e42256d79179c66337761e
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-06 20:11:22 -0300

    Implemens extract route

--- routes/extract.py
+++ routes/extract.py
@@ -1,5 +1,21 @@
 """Extract Module"""
 from app import app
-@app.get('/account/<int:account_id>/extract')
+from models import Transacao
+from schemas import ExtractDepositOut
+
+@app.get("/account/<int:account_id>/extract")
+@app.output(ExtractDepositOut)
 def extract_account(account_id):
-    return {}
+    """Extract endpoint"""
+
+    try:
+        transacao: Transacao = Transacao.query.filter_by(
+            id_conta=account_id,
+        ).all()
+    except Exception as error:
+        return {
+            "message": "Internal error"
+        }, 500
+
+    return transacao
+    # TODO: Add list of transaction in return of schema
